c19aefe3b53164914cfa2a9a8b1707e50d5d555b
woorea <luis@woorea.es>
2013-02-22 09:29:18 +0100

    this is the v2

--- src/es/experiences/HelloWorld.java
+++ src/es/experiences/HelloWorld.java
@@ -6,9 +6,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("******************");
-		System.out.println("* Hola Caracola! *");
-		System.out.println("******************");
+		System.out.println("v2");
 	}
 
 }
